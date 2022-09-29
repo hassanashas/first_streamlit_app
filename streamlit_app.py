2de1b5d83dd4f45109cb8eae8cb070666add307a
hassanashas <67394417+hassanashas@users.noreply.github.com>
2022-09-29 16:36:22 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,9 @@
 import streamlit 
 
 streamlit.title("Welcome to the World of Noobs") 
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
